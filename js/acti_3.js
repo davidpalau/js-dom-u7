487a56684e8639dc6f04bee181e7666b2c9928c7
davidpalau <palau.david@hotmail.com>
2022-11-26 18:52:18 +0100

    Actividad 3 terminada.

--- js/acti_3.js
+++ js/acti_3.js
@@ -4,10 +4,40 @@
 − Lo haremos mediante una cookie que caducará después de un año 
 − Pero primero avisaremos con un cuadro de confirmación, que grabamos cookies en la página. Si el usuario 
 no acepta cookies las borramos y no mostraremos las visitas. Eset mensaje aparecerá al principio. 
-− Ese cuadro sale una vez por sesión 
+− Ese cuadro sale una vez por sesión ;expires=Thu, 01 Jan 1970 00:00:00 UTC";
  */
 
-document.cookie = "gema=valido"
+const fecha = new Date;
+
+let anyo =  fecha.getFullYear() +1
+fecha.setFullYear(anyo);
+
+    let caducidad = fecha.toUTCString();
+    console.log(caducidad)
+
+
+if (document.cookie.length == 0) {
+    document.cookie = "username=1;expires="+caducidad;
+    let confirma = window.confirm("Este sitio utiliza cookie, confirma: ")
+    if (confirma) {
+        document.write(" Visita 1.")
+    } else {
+        document.cookie = "username=1;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+} else {
+    visitacookie = document.cookie.valueOf();
+    visitacookie = visitacookie.split("=");
+    let confirma = window.confirm("cookie, confirma: ")
+    if (confirma) {
+        let visita = Number(parseInt(visitacookie[1]))
+        visita++;
+        document.cookie = "username="+ visita+";expires="+caducidad;
+        document.write(" Visita " + visita+ ".")
+    } else if(confirma==false){
+
+       document.cookie = "username="+";expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+}
 
 console.log(document.cookie)
 
